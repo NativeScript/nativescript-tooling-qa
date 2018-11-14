b138073b705fdcde5972baa600894015a1ce1c07
Dimitar Topuzov <dtopuzov@gmail.com>
2018-11-14 17:34:01 +0200

    fix: IOS in DeviceType enum

--- core/enums/device_type.py
+++ core/enums/device_type.py
@@ -10,7 +10,7 @@ class DeviceType(Enum):
     EMU = 1, 'emulator'
     SIM = 2, 'simulator'
     ANDROID = 3, 'android device'
-    IOS = 3, 'ios device'
+    IOS = 4, 'ios device'
 
     def __str__(self):
         return self.string
