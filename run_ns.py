c6455319269db7c887290bd39638f176db7f8839
dtopuzov <topuzov@progress.com>
2018-12-04 22:02:27 +0200

    chore: do not use flaky

--- run_ns.py
+++ run_ns.py
@@ -8,7 +8,7 @@ from core.log.log import Log
 if __name__ == '__main__':
     run_common.prepare()
     Log.info("Running tests...")
-    arguments = ['nosetests', '-v', '-s', '--nologcapture', '--logging-filter=nose', '--with-xunit', '--with-flaky']
+    arguments = ['nosetests', '-v', '-s', '--nologcapture', '--with-doctest', '--with-xunit']
     for i in sys.argv:
         arguments.append(str(i))
     nose.run(argv=arguments)
