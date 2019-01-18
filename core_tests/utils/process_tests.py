9853f7ed305cffe005997785785381822880a86e
Dimitar Topuzov <dtopuzov@gmail.com>
2019-01-18 14:41:16 +0200

    fix: unit tests

--- core_tests/utils/process_tests.py
+++ core_tests/utils/process_tests.py
@@ -1,26 +1,32 @@
-import time
 import unittest
 
+import time
+
+from core.enums.os_type import OSType
+from core.settings import Settings
 from core.utils.process import Process
 from core.utils.run import run
 
 
 class ProcessTests(unittest.TestCase):
+    if Settings.HOST_OS == OSType.WINDOWS:
+        http_module = 'http.server'
+    else:
+        http_module = 'SimpleHTTPServer'
 
     def test_30_kill_by_port(self):
         port = 4210
         self.start_server(port=port)
         time.sleep(0.5)
-        running = Process.is_running_by_commandline(commandline='http.server')
+        running = Process.is_running_by_commandline(commandline=self.http_module)
         assert running, 'Failed to start simple http server.'
         Process.kill_by_port(port=port)
         time.sleep(0.5)
-        running = Process.is_running_by_commandline(commandline='http.server')
+        running = Process.is_running_by_commandline(commandline=self.http_module)
         assert not running, 'Kill by port failed to kill process.'
 
-    @staticmethod
-    def start_server(port):
-        run(cmd='python -m http.server ' + str(port), wait=False)
+    def start_server(self, port):
+        run(cmd='python -m {0} {1}'.format(self.http_module, str(port)), wait=False)
 
 
 if __name__ == '__main__':
