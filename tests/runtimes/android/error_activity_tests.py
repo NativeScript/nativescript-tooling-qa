cef392b1777cf6d8585e9c583b8fbacd613bcb96
Dimitar Topuzov <dtopuzov@gmail.com>
2019-02-05 17:53:36 +0200

    fix: lint

--- tests/runtimes/android/error_activity_tests.py
+++ tests/runtimes/android/error_activity_tests.py
@@ -36,12 +36,12 @@ class AndroidErrorActivityTests(TnsTest):
 
     def test_200_error_activity_shown_on_error(self):
         Tns.run_android(app_name=APP_NAME, emulator=True, wait=False)
-        self.emu.wait_for_text('Exception', timeout=180, retry_delay=10), 'Error activity not found on device!'
-        self.emu.wait_for_text('Logcat'), 'Error activity not found on device!'
-        self.emu.wait_for_text('Error: Kill the app!'), 'Error activity not found on device!'
+        self.emu.wait_for_text('Exception', timeout=180, retry_delay=10)
+        self.emu.wait_for_text('Logcat')
+        self.emu.wait_for_text('Error: Kill the app!')
         # TODO: Verify stack trace of the error is printed in console (waiting nativescript-tooling-qa/pull/26).
 
     def test_400_no_error_activity_in_release_builds(self):
         Tns.run_android(app_name=APP_NAME, release=True, emulator=True, wait=False)
-        self.emu.wait_for_text('Unfortunately', timeout=180, retry_delay=10), 'No error that app crashed!'
-        self.emu.is_text_visible('Exception'), 'Error activity found on device!'
+        self.emu.wait_for_text('Unfortunately', timeout=180, retry_delay=10)
+        self.emu.is_text_visible('Exception')
