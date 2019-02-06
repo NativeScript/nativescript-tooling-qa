e87ad39bf7cd604f7a79a8826fea9f0be3049ac3
Dimitar Topuzov <dtopuzov@gmail.com>
2019-02-06 12:55:56 +0200

    feat: init and intall tests

--- tests/cli/create/test_init.py
+++ tests/cli/create/test_init.py
@@ -3,7 +3,8 @@ import os
 from core.base_test.tns_test import TnsTest
 from core.enums.os_type import OSType
 from core.settings import Settings
-from core.utils.file_utils import Folder
+from core.utils.file_utils import Folder, File
+from core.utils.npm import Npm
 from products.nativescript.app import App
 from products.nativescript.tns import Tns
 
@@ -29,112 +30,66 @@ class InitAndInstallTests(TnsTest):
 
     def test_203_init_update_existing_file(self):
         self.test_202_init_path()
+
         # Modify existing file
-        for line in fileinput.input(self.app_name + "/package.json", inplace=1):
-            print
-            line.lower().replace(app_identifier, "org.nativescript.TestApp12"),
-        output = File.read(self.app_name + "/package.json")
-        print
-        output
-        assert "org.nativescript.TestApp12" in output
+        package_json = os.path.join(Settings.TEST_RUN_HOME, APP_NAME, 'package.json')
+        File.replace(path=package_json, old_string=APP_NAME, new_string='QApp')
+        assert 'QApp' in File.read(package_json)
 
         # Overwrite changes
         self.test_202_init_path()
 
     def test_204_install_defaults(self):
         self.test_202_init_path()
-        Tns.install(attributes={"--path": self.app_name})
-        assert File.exists(self.app_name + "/platforms/android/build.gradle")
+        result = Tns.exec_command(command='install', path=APP_PATH)
+        self.verify_installed(output=result.output)
 
-        if CURRENT_OS == OSType.OSX:
-            assert File.exists(self.app_name + "/platforms/ios/TestApp.xcodeproj")
-
-    def test_205_install_node_modules(self):
+    def test_205_install_external_packages(self):
         self.test_202_init_path()
 
-        if (USE_YARN == "True"):
-            Npm.install(package="gulp", option="--dev", folder=BaseClass.app_name)
-            Npm.install(package="lodash", folder=BaseClass.app_name)
-        else:
-            Npm.install(package="gulp", option="--save-dev", folder=BaseClass.app_name)
-            Npm.install(package="lodash", option="--save", folder=BaseClass.app_name)
+        # Add packages
+        Npm.install(package='lodash', option='--save', folder=APP_PATH)
+        Npm.install(package='gulp', option='--save-dev', folder=APP_PATH)
+        assert App.is_dependency(app_name=APP_NAME, dependency='lodash')
+        assert App.is_dev_dependency(app_name=APP_NAME, dependency='gulp')
 
-        output = File.read(self.app_name + "/package.json")
-        assert devDependencies in output
-        assert "gulp" in output
-        assert "lodash" in output
+        # Clean node modules
+        Folder.clean(os.path.join(APP_PATH, 'node_modules'))
+        Folder.clean(os.path.join(APP_PATH, 'platforms'))
 
-        Folder.cleanup(self.app_name + '/platforms')
-        assert not File.exists(self.app_name + "/platforms")
-        Folder.cleanup(self.app_name + '/node_modules')
-        assert not File.exists(self.app_name + "/node_modules")
+        # Install and verify common packages
+        result = Tns.exec_command(command='install', path=APP_PATH)
+        self.verify_installed(output=result.output)
 
-        Tns.install(attributes={"--path": self.app_name})
-        assert File.exists(self.app_name + "/node_modules/lodash")
-        assert File.exists(self.app_name + "/node_modules/gulp")
+        # Verify external packages are also installed
+        assert Folder.exists(os.path.join(APP_PATH, 'node_modules', 'lodash'))
+        assert Folder.exists(os.path.join(APP_PATH, 'node_modules', 'gulp'))
 
-        assert File.exists(self.app_name + "/platforms/android/build.gradle")
-
-        if CURRENT_OS == OSType.OSX:
-            assert File.exists(self.app_name + "/platforms/ios/TestApp.xcodeproj")
-
-    def test_300_install_node_modules_if_node_modules_folder_exists(self):
+    def test_300_install_and_prepare(self):
         self.test_202_init_path()
 
-        if (USE_YARN == "True"):
-            Npm.install(package="gulp", option="--dev", folder=BaseClass.app_name)
-            Npm.install(package="lodash", folder=BaseClass.app_name)
-        else:
-            Npm.install(package="gulp", option="--save-dev", folder=BaseClass.app_name)
-            Npm.install(package="lodash", option="--save", folder=BaseClass.app_name)
+        # Add packages
+        Npm.install(package='lodash', option='--save', folder=APP_PATH)
+        Npm.install(package='gulp', option='--save-dev', folder=APP_PATH)
+        assert App.is_dependency(app_name=APP_NAME, dependency='lodash')
+        assert App.is_dev_dependency(app_name=APP_NAME, dependency='gulp')
 
-        output = File.read(self.app_name + "/package.json")
-        assert devDependencies in output
-        assert "gulp" in output
-        assert "lodash" in output
+        # Call install and verify it do not fail if everything is already installed
+        result = Tns.exec_command(command='install', path=APP_PATH)
+        self.verify_installed(output=result.output)
+        assert Folder.exists(os.path.join(APP_PATH, 'node_modules', 'lodash'))
+        assert Folder.exists(os.path.join(APP_PATH, 'node_modules', 'gulp'))
 
-        Tns.install(attributes={"--path": self.app_name})
-        assert File.exists(self.app_name + "/node_modules/lodash")
-        assert File.exists(self.app_name + "/node_modules/gulp")
-        assert File.exists(self.app_name + "/platforms/android/build.gradle")
-        if CURRENT_OS == OSType.OSX:
-            assert File.exists(self.app_name + "/platforms/ios/TestApp.xcodeproj")
+        # Copy app folder and app resources
+        Folder.copy(source=os.path.join(Settings.TEST_RUN_HOME, 'assets', 'template-min', 'app'), target=APP_PATH)
 
-    def test_301_install_and_prepare(self):
-        self.test_202_init_path()
-        Tns.platform_add_android(attributes={"--path": self.app_name}, version="next")
+        # Prepare project
+        Tns.prepare_android(app_name=APP_NAME)
+        if Settings.HOST_OS == OSType.OSX:
+            Tns.prepare_ios(app_name=APP_NAME)
 
-        Npm.install(package='', folder=BaseClass.app_name)
-        if (USE_YARN == "True"):
-            Npm.install(package="gulp", option="--dev", folder=BaseClass.app_name)
-            Npm.install(package="lodash", folder=BaseClass.app_name)
-        else:
-            Npm.install(package="gulp", option="--save-dev", folder=BaseClass.app_name)
-            Npm.install(package="lodash", option="--save", folder=BaseClass.app_name)
-
-        source = os.path.join(SUT_FOLDER, "template-hello-world", "app")
-        dest = os.path.join(self.app_name, "app")
-        Folder.copy(source, dest)
-
-        output = File.read(self.app_name + "/package.json")
-        assert devDependencies in output
-        assert "gulp" in output
-        assert "lodash" in output
-
-        Tns.install(attributes={"--path": self.app_name})
-        assert File.exists(self.app_name + "/node_modules/lodash")
-        assert File.exists(self.app_name + "/node_modules/gulp")
-        assert File.exists(self.app_name + "/platforms/android/build.gradle")
-
-        Tns.prepare_android(attributes={"--path": self.app_name})
-        assert File.exists(os.path.join(self.app_name, TnsAsserts.PLATFORM_ANDROID_NPM_MODULES_PATH, "lodash"))
-        assert not File.exists(os.path.join(self.app_name, TnsAsserts.PLATFORM_ANDROID_NPM_MODULES_PATH, "gulp"))
-
-        if CURRENT_OS == OSType.OSX:
-            assert File.exists(self.app_name + "/platforms/ios/TestApp.xcodeproj")
-            Tns.prepare_ios(attributes={"--path": self.app_name})
-            assert File.exists(os.path.join(self.app_name, TnsAsserts.PLATFORM_ANDROID_NPM_MODULES_PATH, "lodash"))
-            assert not File.exists(os.path.join(self.app_name, TnsAsserts.PLATFORM_ANDROID_NPM_MODULES_PATH, "gulp"))
+        # Verify lodash and gulp and are in platforms
+        # TODO: Will finish is later
 
     def test_400_install_in_not_existing_folder(self):
         result = Tns.exec_command(command='install', path=APP_NAME)
@@ -157,3 +112,20 @@ class InitAndInstallTests(TnsTest):
         if Settings.HOST_OS == OSType.OSX:
             assert json['nativescript']['tns-ios']['version'] is not None
         assert json['dependencies']['tns-core-modules'] is not None
+
+    def verify_installed(self, output):
+        # Verify output
+        assert 'Copying template files' in output
+        assert 'Platform android successfully added' in output
+        if Settings.HOST_OS == OSType.OSX:
+            assert 'Platform ios successfully added' in output
+
+        # Verify files
+        assert Folder.exists(os.path.join(APP_PATH, 'node_modules', 'tns-core-modules'))
+        assert Folder.exists(os.path.join(APP_PATH, 'platforms', 'android'))
+        assert File.exists(os.path.join(APP_PATH, 'platforms', 'android', 'build.gradle'))
+        if Settings.HOST_OS == OSType.OSX:
+            assert Folder.exists(os.path.join(APP_PATH, 'platforms', 'ios'))
+            assert File.exists(os.path.join(APP_PATH, 'platforms', 'ios', 'TestApp.xcodeproj'))
+
+        # App resources not created, see https://github.com/NativeScript/nativescript-cli/issues/4341
