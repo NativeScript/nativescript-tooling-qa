8c401ab003dcde02bef87e9e799af09702300cdb
dtopuzov <dtopuzov@gmail.com>
2019-02-10 08:44:52 +0200

    refactor: template tests based on TnsRunTest

--- tests/cli/run/templates/master_detail_ng_tests.py
+++ tests/cli/run/templates/master_detail_ng_tests.py
@@ -1,33 +1,24 @@
 import os
 import unittest
 
-from core.base_test.tns_test import TnsTest
+from core.base_test.tns_run_test import TnsRunTest
 from core.enums.os_type import OSType
 from core.enums.platform_type import Platform
 from core.settings import Settings
-from core.utils.device.adb import Adb
-from core.utils.device.device_manager import DeviceManager
 from core.utils.file_utils import Folder
 from data.sync.master_details_ng import sync_master_detail_ng
 from data.templates import Template
 from products.nativescript.tns import Tns
 
 
-class TnsRunMasterDetailTests(TnsTest):
+class TnsRunMasterDetailTests(TnsRunTest):
     app_name = Settings.AppName.DEFAULT
     source_project_dir = os.path.join(Settings.TEST_RUN_HOME, app_name)
     target_project_dir = os.path.join(Settings.TEST_RUN_HOME, 'data', 'temp', app_name)
-    emu = None
-    sim = None
 
     @classmethod
     def setUpClass(cls):
-        TnsTest.setUpClass()
-
-        # Boot emulator and simulator
-        cls.emu = DeviceManager.Emulator.ensure_available(Settings.Emulators.DEFAULT)
-        if Settings.HOST_OS == OSType.OSX:
-            cls.sim = DeviceManager.Simulator.ensure_available(Settings.Simulators.DEFAULT)
+        TnsRunTest.setUpClass()
 
         # Create app
         Tns.create(app_name=cls.app_name, template=Template.MASTER_DETAIL_NG.local_package, update=True)
@@ -39,9 +30,7 @@ class TnsRunMasterDetailTests(TnsTest):
         Folder.copy(source=cls.source_project_dir, target=cls.target_project_dir)
 
     def setUp(self):
-        TnsTest.setUp(self)
-        Adb.open_home(self.emu.id)
-
+        TnsRunTest.setUp(self)
         # "src" folder of TestApp will be restored before each test.
         # This will ensure failures in one test do not cause common failures.
         source_src = os.path.join(self.target_project_dir, 'src')
@@ -49,8 +38,6 @@ class TnsRunMasterDetailTests(TnsTest):
         Folder.clean(target_src)
         Folder.copy(source=source_src, target=target_src)
 
-
-class RunAndroidMasterDetailNGTests(TnsRunMasterDetailTests):
     def test_100_run_android(self):
         sync_master_detail_ng(self.app_name, Platform.ANDROID, self.emu)
 
@@ -66,20 +53,22 @@ class RunAndroidMasterDetailNGTests(TnsRunMasterDetailTests):
     def test_320_run_android_bundle_aot_and_uglify(self):
         sync_master_detail_ng(self.app_name, Platform.ANDROID, self.emu, bundle=True, aot=True, uglify=True)
 
-
-@unittest.skipIf(Settings.HOST_OS != OSType.OSX, 'iOS tests can be executed only on macOS.')
-class RunIOSMasterDetailNGTests(TnsRunMasterDetailTests):
+    @unittest.skipIf(Settings.HOST_OS != OSType.OSX, 'iOS tests can be executed only on macOS.')
     def test_100_run_ios(self):
         sync_master_detail_ng(self.app_name, Platform.IOS, self.sim)
 
+    @unittest.skipIf(Settings.HOST_OS != OSType.OSX, 'iOS tests can be executed only on macOS.')
     def test_200_run_ios_bundle(self):
         sync_master_detail_ng(self.app_name, Platform.IOS, self.sim, bundle=True)
 
+    @unittest.skipIf(Settings.HOST_OS != OSType.OSX, 'iOS tests can be executed only on macOS.')
     def test_300_run_ios_bundle_aot(self):
         sync_master_detail_ng(self.app_name, Platform.IOS, self.sim, bundle=True, aot=True)
 
+    @unittest.skipIf(Settings.HOST_OS != OSType.OSX, 'iOS tests can be executed only on macOS.')
     def test_310_run_ios_bundle_uglify(self):
         sync_master_detail_ng(self.app_name, Platform.IOS, self.sim, bundle=True, uglify=True)
 
+    @unittest.skipIf(Settings.HOST_OS != OSType.OSX, 'iOS tests can be executed only on macOS.')
     def test_320_run_ios_bundle_aot_and_uglify(self):
         sync_master_detail_ng(self.app_name, Platform.IOS, self.sim, bundle=True, aot=True, uglify=True)
