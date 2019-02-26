e802d6db2b0a25b3afde960ffc7ea42ccf380b0a
Dimitar Topuzov <dtopuzov@gmail.com>
2019-02-26 17:33:57 +0200

    feat: tests for ng g commands

---
+++ tests/code_sharing/test_ng_generate_e2e.py
@@ -0,0 +1,85 @@
+import os
+import unittest
+
+from core.base_test.tns_run_test import TnsRunTest
+from core.enums.app_type import AppType
+from core.enums.os_type import OSType
+from core.enums.platform_type import Platform
+from core.settings import Settings
+from core.utils.file_utils import Folder, File
+from products.angular.ng import NG, NS_SCHEMATICS
+from products.nativescript.tns import Tns
+from products.nativescript.tns_assert import TnsAssert
+from products.nativescript.tns_logs import TnsLogs
+from products.nativescript.tns_paths import TnsPaths
+
+
+class NGGenE2ETestsNS(TnsRunTest):
+    ns_app_name = Settings.AppName.DEFAULT
+    shared_app_name = 'SharedApp'
+    ns_app_path = TnsPaths.get_app_path(app_name=ns_app_name)
+    shared_app_path = TnsPaths.get_app_path(app_name=shared_app_name)
+
+    @classmethod
+    def setUpClass(cls):
+        TnsRunTest.setUpClass()
+        NG.kill()
+
+    def setUp(self):
+        TnsRunTest.setUpClass()
+        NG.kill()
+
+    def tearDown(self):
+        NG.kill()
+        TnsRunTest.tearDown(self)
+
+    def test_100_ns_generate_during_run_android(self):
+        NGGenE2ETestsNS.workflow(app_name=self.ns_app_name, device=self.emu, platform=Platform.ANDROID, shared=False)
+
+    @unittest.skipIf(Settings.HOST_OS != OSType.OSX, 'Skip iOS tests on non macOS machines.')
+    def test_100_ns_generate_during_run_ios(self):
+        NGGenE2ETestsNS.workflow(app_name=self.ns_app_name, device=self.sim, platform=Platform.IOS, shared=False)
+
+    def test_200_shared_generate_during_run_android(self):
+        NGGenE2ETestsNS.workflow(app_name=self.ns_app_name, device=self.emu, platform=Platform.ANDROID, shared=True)
+
+    @unittest.skipIf(Settings.HOST_OS != OSType.OSX, 'Skip iOS tests on non macOS machines.')
+    def test_200_shared_generate_during_run_ios(self):
+        NGGenE2ETestsNS.workflow(app_name=self.ns_app_name, device=self.sim, platform=Platform.IOS, shared=True)
+
+    @staticmethod
+    def workflow(app_name, device, platform, shared):
+        # Create an app
+        app_path = TnsPaths.get_app_path(app_name=app_name)
+        Folder.clean(app_path)
+        NG.new(collection=NS_SCHEMATICS, project=app_name, shared=shared)
+        TnsAssert.created(app_name=app_name, app_data=None)
+
+        # Run app initially
+        result = Tns.run_android(app_name=app_name, emulator=True, hmr=True)
+        strings = TnsLogs.run_messages(app_name=app_name, platform=platform, bundle=True, hmr=True, app_type=AppType.NG)
+        TnsLogs.wait_for_log(log_file=result.log_file, string_list=strings, timeout=300)
+        device.wait_for_text(text='TAP')
+
+        # Generate module and component
+        NG.exec_command(command='g m module-test', cwd=app_path)
+        NG.exec_command(command='g c module-test/component-test', cwd=app_path)
+
+        # Update app.modules.ts
+        app_module = os.path.join(app_path, 'app', 'app.module.ts')
+        old_string = "import { HomeComponent } from './home/home.component';"
+        new_string = "import { ComponentTestComponent } from './module-test/component-test/component-test.component';"
+        File.replace(path=app_module, old_string=old_string, new_string=new_string)
+        File.replace(path=app_module, old_string='HomeComponent,', new_string='ComponentTestComponent,')
+
+        # Update app-routing.module.ts
+        app_routing_module = os.path.join(app_path, 'app', 'app-routing.module.ts')
+        old_string = "import { HomeComponent } from './home/home.component';"
+        new_string = "import { ComponentTestComponent } from './module-test/component-test/component-test.component';"
+        File.replace(path=app_routing_module, old_string=old_string, new_string=new_string)
+        File.replace(path=app_routing_module, old_string='HomeComponent', new_string='ComponentTestComponent')
+
+        # Verify app is updated
+        logs = [app_module, app_routing_module, 'Successfully synced application']
+        TnsLogs.wait_for_log(log_file=result.log_file, string_list=logs, timeout=300)
+        device.wait_for_text(text='component-test works!')
---
+++ tests/code_sharing/test_ng_generate_shared.py
@@ -0,0 +1,77 @@
+"""
+Tests for `ng g` in context of shared application.
+"""
+import unittest
+
+from core.base_test.tns_run_test import TnsRunTest
+from core.base_test.tns_test import TnsTest
+from core.settings import Settings
+from core.utils.file_utils import Folder
+from products.angular.ng import NG, NS_SCHEMATICS
+from products.nativescript.tns_assert import TnsAssert
+from products.nativescript.tns_paths import TnsPaths
+
+
+# noinspection PyMethodMayBeStatic
+class NGGenerateSharedTests(TnsTest):
+    app_name = Settings.AppName.DEFAULT
+    app_path = TnsPaths.get_app_path(app_name=app_name)
+
+    @classmethod
+    def setUpClass(cls):
+        TnsTest.setUpClass()
+        NG.kill()
+        Folder.clean(cls.app_path)
+
+        # Create app
+        NG.new(collection=NS_SCHEMATICS, project=cls.app_name, shared=True)
+        TnsAssert.created(app_name=cls.app_name, app_data=None)
+
+    def setUp(self):
+        TnsTest.setUpClass()
+        NG.kill()
+
+    def tearDown(self):
+        NG.kill()
+        TnsTest.tearDown(self)
+
+    def test_001_generate_component(self):
+        result = NG.exec_command(command='g c component-test', cwd=self.app_path)
+        assert 'CREATE src/app/component-test/component-test.component.html' in result.output
+        assert 'CREATE src/app/component-test/component-test.component.ts' in result.output
+        assert 'CREATE src/app/component-test/component-test.component.css' in result.output
+        assert 'CREATE src/app/component-test/component-test.component.tns.css' in result.output
+        assert 'CREATE src/app/component-test/component-test.component.tns.html' in result.output
+        assert 'UPDATE src/app/app.module.ts' in result.output
+        assert 'UPDATE src/app/app.module.tns.ts' in result.output
+
+    def test_002_generate_module(self):
+        result = NG.exec_command(command='g m module-test', cwd=self.app_path)
+        assert 'CREATE src/app/module-test/module-test.module.ts' in result.output
+        assert 'CREATE src/app/module-test/module-test.module.tns.ts' in result.output
+        assert 'CREATE src/app/module-test/module-test.common.ts' in result.output
+
+    def test_003_generate_component_in_existing_modules(self):
+        result = NG.exec_command(command='g m module-test2', cwd=self.app_path)
+        assert 'CREATE src/app/module-test2/module-test2.module.ts' in result.output
+        assert 'CREATE src/app/module-test2/module-test2.module.tns.ts' in result.output
+        assert 'CREATE src/app/module-test2/module-test2.common.ts' in result.output
+
+        result = NG.exec_command(command='g c module-test2/component-name', cwd=self.app_path)
+        assert 'CREATE src/app/module-test2/component-name/component-name.component.html' in result.output
+        assert 'CREATE src/app/module-test2/component-name/component-name.component.ts' in result.output
+        assert 'CREATE src/app/module-test2/component-name/component-name.component.css' in result.output
+        assert 'CREATE src/app/module-test2/component-name/component-name.component.tns.css' in result.output
+        assert 'CREATE src/app/module-test2/component-name/component-name.component.tns.html' in result.output
+        assert 'UPDATE src/app/module-test2/module-test2.module.ts' in result.output
+        assert 'UPDATE src/app/module-test2/module-test2.module.tns.ts' in result.output
+
+    @unittest.skip('Skip because of https://github.com/NativeScript/nativescript-schematics/issues/194')
+    def test_004_generate_master_detail(self):
+        result = NG.exec_command(command='g master-detail --master=dogs --detail=dog', cwd=self.app_path)
+        assert 'CREATE src/app/dogs/dog-detail/dog-detail.component.html' in result.output
+        assert 'CREATE src/app/dogs/dog-detail/dog-detail.component.tns.html' in result.output
+        assert 'CREATE src/app/dogs/dogs/dogs.component.html' in result.output
+        assert 'CREATE src/app/dogs/dogs/dogs.component.tns.html' in result.output
+        assert 'data.service.ts' in result.output
+        assert 'dogs.module.ts' in result.output
