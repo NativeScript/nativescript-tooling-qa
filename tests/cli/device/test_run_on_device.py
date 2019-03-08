74c736112dfd879bb02d4ac819e886261e53e305
Dimitar Topuzov <dtopuzov@gmail.com>
2019-03-08 06:06:26 -0800

    chore: add device ids to log checks

--- tests/cli/device/test_run_on_device.py
+++ tests/cli/device/test_run_on_device.py
@@ -60,6 +60,8 @@ class TnsRunOnDevices(TnsDeviceTest):
         # Wait for logs
         strings = TnsLogs.run_messages(app_name=self.app_name, platform=Platform.ANDROID, run_type=RunType.FULL,
                                        bundle=True, hmr=True)
+        for device in DeviceManager.get_devices(device_type=any):
+            strings.append(device.id)
         TnsLogs.wait_for_log(log_file=result.log_file, string_list=strings, timeout=300)
 
         # Verify it looks properly
