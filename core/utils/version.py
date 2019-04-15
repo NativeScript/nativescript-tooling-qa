6e51ad5f034709df548047dc785a5090f1c8ea3f
Dimitar Topuzov <dtopuzov@gmail.com>
2019-04-15 03:47:46 -0700

    chore: remove flaky logic in Adb.get_screen() (#127)
    We tried to optimize get screneshot by getting it as stream and forward to file on host machines.
    This is faster than get screenshot on sdcard and pull it.
    
    We found on some machines and our assumption was it it because of update to macOS Mohave,
    It turned out that that is not the reason.
    
    Fall back to old logic that get screenshot on sdcard and pull it.
    It is slower, but looks to be more stable.

--- core/utils/version.py
+++ core/utils/version.py
@@ -1,10 +1,6 @@
 """
 Version utils
 """
-import platform
-
-from core.enums.os_type import OSType
-from core.settings import Settings
 
 
 class Version(object):
@@ -20,18 +16,3 @@ class Version(object):
             return float(split[0] + '.' + split[1])
         else:
             return float(split[0])
-
-    @staticmethod
-    def is_os_mojave():
-        """
-        Check whether a machine OS is Mac Mojave.
-        :return: boolean.
-        """
-        is_machine_mojave = False
-        if Settings.HOST_OS is OSType.OSX:
-            version, _, _ = platform.mac_ver()
-            version = float('.'.join(version.split('.')[:2]))
-            if version == 10.14:
-                is_machine_mojave = True
-
-        return is_machine_mojave
