86bd4668588b41eb2107c9e8f290cf571aa1b850
Natalia-Hristova <natalia.hristova@telerik.com>
2019-05-07 10:15:32 +0300

    feat: add test for adding swift files

---
+++ assets/runtime/ios/files/ios-runtime-1131/src/SwiftClass.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+@objc class SwiftClass: NSObject {
+    @objc public let number:Int = 123
+    
+    @objc public func swiftMethod () -> String {
+        return "GREAT!"
+    }
+}
