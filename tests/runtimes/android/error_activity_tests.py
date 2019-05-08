7252cf5b1c416e5695c4a6dc4c95db09695c7067
vhristov5555 <vasil.hristov@progress.com>
2019-05-08 11:23:30 +0300

    Change the wait logic for logs in error activity tests.

--- tests/runtimes/android/error_activity_tests.py
+++ tests/runtimes/android/error_activity_tests.py
@@ -35,11 +35,11 @@ class AndroidErrorActivityTests(TnsTest):
 
         # Break the app to test error activity
         # add workaround with for-cycle for https://github.com/NativeScript/nativescript-cli/issues/3812
-        code = 'var e = new Date().getTime() + 3000; while (new Date().getTime() <= e) {} '
+        wait_code = 'var e = new Date().getTime() + 3000; while (new Date().getTime() <= e) {} '
         exception_code = 'throw new Error("Kill the app!");'
         change = ChangeSet(file_path=os.path.join(Settings.TEST_RUN_HOME, APP_NAME, 'app', 'app.js'),
                            old_value='application.run({ moduleName: "app-root" });',
-                           new_value=code + exception_code)
+                           new_value=wait_code + exception_code)
         Sync.replace(app_name=APP_NAME, change_set=change)
 
         # Verify logs and screen
