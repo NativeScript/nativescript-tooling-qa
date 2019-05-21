d9b5c83ab83ce3d3f0a8552e6c1fa14086cd012b
miroslavaivanova <miroslava.ivanova@progress.com>
2019-05-21 13:52:30 +0300

    build tests

--- tests/cli/build/build_tests.py
+++ tests/cli/build/build_tests.py
@@ -30,6 +30,8 @@ class BuildTests(TnsTest):
         Tns.create(cls.app_name_with_space, update=False, template=Template.HELLO_WORLD_JS.local_package)
         Folder.clean(os.path.join(cls.app_name, 'hooks'))
         Folder.clean(os.path.join(cls.app_name, 'node_modules'))
+        Folder.clean(os.path.join(cls.app_name_with_space, 'hooks'))
+        Folder.clean(os.path.join(cls.app_name_with_space, 'node_modules'))
         Tns.platform_add_android(cls.app_name, framework_path=Settings.Android.FRAMEWORK_PATH)
         Tns.platform_add_android(app_name='"' + cls.app_name_with_space + '"',
                                  framework_path=Settings.Android.FRAMEWORK_PATH, verify=False)
@@ -51,100 +53,100 @@ class BuildTests(TnsTest):
         Folder.clean(cls.app_temp_path)
         Folder.clean(cls.app_name_with_space)
 
-    # def test_001_build_android(self):
-    #     Tns.build_android(self.app_name)
-    #     assert not File.exists(os.path.join(TnsPaths.get_platforms_android_folder(self.app_name), '*.plist'))
-    #     assert not File.exists(os.path.join(TnsPaths.get_platforms_android_folder(self.app_name), '*.android.js'))
-    #     assert not File.exists(os.path.join(TnsPaths.get_platforms_android_folder(self.app_name), '*.ios.js'))
-    #
-    #     src = os.path.join(self.app_name, 'app', 'app.js')
-    #     dest_1 = os.path.join(self.app_name, 'app', 'new.android.js')
-    #     dest_2 = os.path.join(self.app_name, 'app', 'new.ios.js')
-    #     File.copy(src, dest_1)
-    #     File.copy(src, dest_2)
-    #
-    #     result = Tns.build_android(self.app_name)
-    #     assert "Gradle build..." in result.output, "Gradle build not called."
-    #     assert result.output.count("Gradle build...") == 1, "Only one gradle build is triggered."
-    #
-    #     assert not File.exists(os.path.join(TnsPaths.get_platforms_android_folder(self.app_name), '*.plist'))
-    #     assert not File.exists(os.path.join(TnsPaths.get_platforms_android_folder(self.app_name), '*.android.js'))
-    #     assert not File.exists(os.path.join(TnsPaths.get_platforms_android_folder(self.app_name), '*.ios.js'))
-    #
-    #     # Verify apk does not contain aar files
-    #     archive = os.path.join(TnsPaths.get_apk_path(self.app_name), self.debug_apk)
-    #     File.unzip(archive, 'temp')
-    #     # Clean META-INF folder. It contains com.android.support.... files which are expected to be there due to
-    #     # https://github.com/NativeScript/nativescript-cli/pull/3923
-    #     Folder.clean(os.path.join(self.app_name, 'temp', 'META-INF'))
-    #     temp_folder = os.path.join(self.app_name, 'temp')
-    #     assert not File.pattern_exists(temp_folder, '*.aar')
-    #     assert not File.pattern_exists(temp_folder, '*.plist')
-    #     assert not File.pattern_exists(temp_folder, '*.android.*')
-    #     assert not File.pattern_exists(temp_folder, '*.ios.*')
-    #     Folder.clean(temp_folder)
-    #
-    #     # Verify incremental native build
-    #     result = Tns.exec_command(command='build --clean', bundle=True, path=self.app_name,
-    #                               platform=Platform.ANDROID)
-    #     assert "Gradle clean..." in result.output, "Gradle clean is not called."
-    #     assert "Gradle build..." in result.output, "Gradle build is not called."
-    #     assert result.output.count("Gradle build...") == 1, "More than 1 gradle build is triggered."
-    #
-    # def test_002_build_android_release(self):
-    #     Tns.build_android(self.app_name, bundle=True, release=True)
-    #
-    #     # Configs are respected
-    #     assert File.exists(os.path.join(TnsPaths.get_apk_path(self.app_name), 'release', self.release_apk))
-    #
-    #     # Create zip
-    #     command = "tar -czf " + self.app_name + "/app/app.tar.gz " + self.app_name + "/app/app.js"
-    #     run(command, wait=True)
-    #     assert File.exists(os.path.join(self.app_name, 'app', 'app.tar.gz'))
-    #
-    # def test_301_build_project_with_space_release(self):
-    #
-    #     # Ensure ANDROID_KEYSTORE_PATH contain spaces (verification for CLI issue 2650)
-    #     Folder.create("with space")
-    #     file_name = os.path.basename(Settings.Android.ANDROID_KEYSTORE_PATH)
-    #     cert_with_space_path = os.path.join("with space", file_name)
-    #     File.copy(Settings.Android.ANDROID_KEYSTORE_PATH, cert_with_space_path)
-    #
-    #     Tns.build_android(app_name='"' + self.app_name_with_space + '"', bundle=True, release=True)
-    #     output = File.read(os.path.join(self.app_name_with_space, "package.json"))
-    #     assert self.app_identifier in output.lower()
-    #
-    #     output = File.read(os.path.join(TnsPaths.get_platforms_android_src_main_path(self.app_name_with_space),
-    #                                     'AndroidManifest.xml'))
-    #     assert self.app_identifier in output.lower()
-    #
-    # def test_302_build_project_with_space_debug_with_plugin(self):
-    #     Tns.platform_remove(app_name='"' + self.app_name_with_space + '"', platform=Platform.ANDROID)
-    #     Npm.install(package='nativescript-mapbox', option='--save', folder=self.app_name_with_space)
-    #     result = Tns.build_android(app_name='"' + self.app_name_with_space + '"', bundle=True)
-    #     assert "Project successfully built" in result.output
-    #
-    # def test_310_build_android_with_custom_compile_sdk_new(self):
-    #     Tns.platform_remove(self.app_name, platform=Platform.ANDROID)
-    #     Tns.platform_add_android(self.app_name, framework_path=Settings.Android.FRAMEWORK_PATH)
-    #     Tns.exec_command(command='build --compileSdk 28', path=self.app_name,
-    #                      platform=Platform.ANDROID, bundle=True)
-    #
-    #     File.delete(self.debug_apk)
-    #     Tns.exec_command(command='build --copy-to ./', path=self.app_name,
-    #                      platform=Platform.ANDROID, bundle=True)
-    #     assert File.exists(self.debug_apk)
-    #     File.delete(self.debug_apk)
-    #
-    # def test_441_android_typings(self):
-    #     Tns.exec_command(command='build --androidTypings', path=self.app_name,
-    #                      platform=Platform.ANDROID, bundle=True)
-    #     assert File.exists(os.path.join(self.app_name, 'android.d.ts'))
-    #     assert File.exists(os.path.join(self.app_name, 'android-declarations.d.ts'))
+    def test_001_build_android(self):
+        Tns.build_android(self.app_name)
+        assert not File.exists(os.path.join(TnsPaths.get_platforms_android_folder(self.app_name), '*.plist'))
+        assert not File.exists(os.path.join(TnsPaths.get_platforms_android_folder(self.app_name), '*.android.js'))
+        assert not File.exists(os.path.join(TnsPaths.get_platforms_android_folder(self.app_name), '*.ios.js'))
+
+        src = os.path.join(self.app_name, 'app', 'app.js')
+        dest_1 = os.path.join(self.app_name, 'app', 'new.android.js')
+        dest_2 = os.path.join(self.app_name, 'app', 'new.ios.js')
+        File.copy(src, dest_1)
+        File.copy(src, dest_2)
+
+        result = Tns.build_android(self.app_name)
+        assert "Gradle build..." in result.output, "Gradle build not called."
+        assert result.output.count("Gradle build...") == 1, "Only one gradle build is triggered."
+
+        assert not File.exists(os.path.join(TnsPaths.get_platforms_android_folder(self.app_name), '*.plist'))
+        assert not File.exists(os.path.join(TnsPaths.get_platforms_android_folder(self.app_name), '*.android.js'))
+        assert not File.exists(os.path.join(TnsPaths.get_platforms_android_folder(self.app_name), '*.ios.js'))
+
+        # Verify apk does not contain aar files
+        archive = os.path.join(TnsPaths.get_apk_path(self.app_name), self.debug_apk)
+        File.unzip(archive, 'temp')
+        # Clean META-INF folder. It contains com.android.support.... files which are expected to be there due to
+        # https://github.com/NativeScript/nativescript-cli/pull/3923
+        Folder.clean(os.path.join(self.app_name, 'temp', 'META-INF'))
+        temp_folder = os.path.join(self.app_name, 'temp')
+        assert not File.pattern_exists(temp_folder, '*.aar')
+        assert not File.pattern_exists(temp_folder, '*.plist')
+        assert not File.pattern_exists(temp_folder, '*.android.*')
+        assert not File.pattern_exists(temp_folder, '*.ios.*')
+        Folder.clean(temp_folder)
+
+        # Verify incremental native build
+        result = Tns.exec_command(command='build --clean', bundle=True, path=self.app_name,
+                                  platform=Platform.ANDROID)
+        assert "Gradle clean..." in result.output, "Gradle clean is not called."
+        assert "Gradle build..." in result.output, "Gradle build is not called."
+        assert result.output.count("Gradle build...") == 1, "More than 1 gradle build is triggered."
+
+    def test_002_build_android_release(self):
+        Tns.build_android(self.app_name, release=True)
+
+        # Configs are respected
+        assert File.exists(os.path.join(TnsPaths.get_apk_path(self.app_name), 'release', self.release_apk))
+
+        # Create zip
+        command = "tar -czf " + self.app_name + "/app/app.tar.gz " + self.app_name + "/app/app.js"
+        run(command, wait=True)
+        assert File.exists(os.path.join(self.app_name, 'app', 'app.tar.gz'))
+
+    def test_301_build_project_with_space_release(self):
+
+        # Ensure ANDROID_KEYSTORE_PATH contain spaces (verification for CLI issue 2650)
+        Folder.create("with space")
+        file_name = os.path.basename(Settings.Android.ANDROID_KEYSTORE_PATH)
+        cert_with_space_path = os.path.join("with space", file_name)
+        File.copy(Settings.Android.ANDROID_KEYSTORE_PATH, cert_with_space_path)
+
+        Tns.build_android(app_name='"' + self.app_name_with_space + '"', release=True)
+        output = File.read(os.path.join(self.app_name_with_space, "package.json"))
+        assert self.app_identifier in output.lower()
+
+        output = File.read(os.path.join(TnsPaths.get_platforms_android_src_main_path(self.app_name_with_space),
+                                        'AndroidManifest.xml'))
+        assert self.app_identifier in output.lower()
+
+    def test_302_build_project_with_space_debug_with_plugin(self):
+        Tns.platform_remove(app_name='"' + self.app_name_with_space + '"', platform=Platform.ANDROID)
+        Npm.install(package='nativescript-mapbox', option='--save', folder=self.app_name_with_space)
+        result = Tns.build_android(app_name='"' + self.app_name_with_space + '"')
+        assert "Project successfully built" in result.output
+
+    def test_310_build_android_with_custom_compile_sdk_new(self):
+        Tns.platform_remove(self.app_name, platform=Platform.ANDROID)
+        Tns.platform_add_android(self.app_name, framework_path=Settings.Android.FRAMEWORK_PATH)
+        Tns.exec_command(command='build --compileSdk 28', path=self.app_name,
+                         platform=Platform.ANDROID, bundle=True)
+
+        File.delete(self.debug_apk)
+        Tns.exec_command(command='build --copy-to ./', path=self.app_name,
+                         platform=Platform.ANDROID, bundle=True)
+        assert File.exists(self.debug_apk)
+        File.delete(self.debug_apk)
+
+    def test_441_android_typings(self):
+        Tns.exec_command(command='build --androidTypings', path=self.app_name,
+                         platform=Platform.ANDROID, bundle=True)
+        assert File.exists(os.path.join(self.app_name, 'android.d.ts'))
+        assert File.exists(os.path.join(self.app_name, 'android-declarations.d.ts'))
 
     def test_450_resources_update_android(self):
-        target_app = os.path.join(self.app_name)
-        source_app = os.path.join(TEST_RUN_HOME, 'assets', 'apps', 'test-app-js-41')
+        target_app = os.path.join(self.app_name, 'app', 'App_Resources')
+        source_app = os.path.join(TEST_RUN_HOME, 'assets', 'apps', 'test-app-js-41', 'app', 'App_Resources')
         Folder.clean(target_app)
         Folder.copy(source_app, target_app)
 
@@ -167,8 +169,8 @@ class BuildTests(TnsTest):
             os.path.join(TnsPaths.get_platforms_android_src_main_path(self.app_name), 'AndroidManifest.xml'))
 
     def test_451_resources_update(self):
-        target_app = os.path.join(self.app_name)
-        source_app = os.path.join(TEST_RUN_HOME, 'assets', 'apps', 'test-app-js-41')
+        target_app = os.path.join(self.app_name, 'app', 'App_Resources')
+        source_app = os.path.join(TEST_RUN_HOME, 'assets', 'apps', 'test-app-js-41', 'app', 'App_Resources')
         Folder.clean(target_app)
         Folder.copy(source_app, target_app)
 
