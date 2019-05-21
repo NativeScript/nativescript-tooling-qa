48b46e04d3a47a53f8b5967cee3fc8d9e44ff318
VladimirAmiorkov <vladimir.amiorkov@progress.com>
2019-05-21 16:36:13 +0300

    chore: change the format of output to json

--- products/nativescript/market_helpers.py
+++ products/nativescript/market_helpers.py
@@ -2,9 +2,25 @@ import json
 import os
 import urllib
 
+from datetime import date
 from core.log.log import Log
 from core.settings import Settings
 
+class FlavorStatus(object):
+    def setAndroid(self, value):
+        self.android = value
+    def getAndroid(self):
+        return self.android
+
+    def setIOS(self, value):
+        self.ios = value
+    def getIOS(self):
+        return self.ios
+        
+    def setSlow(self, value):
+        self.slow = value
+    def getSlow(self):
+        return self.slow
 
 class Market(object):
 
@@ -56,20 +72,85 @@ class Market(object):
 
         return testing_data
 
+
+    def get_preserved_data():
+        file_path = os.path.join(Settings.TEST_RUN_HOME, 'results.json')
+        data = None
+        if os.path.isfile(file_path):
+            with open(file_path, "r") as jsonFile:
+                data = json.load(jsonFile)
+        else:
+            with open(file_path, "w") as newFile:
+                Log.info("Results.json file created")
+
+        return data
+
+    def serialize(obj):
+        if isinstance(obj, date):
+            serial = obj.isoformat()
+            return serial
+
+        return obj.__dict__
+
     @staticmethod
     def preserve_data(record):
-        # from string to dict - some = ast.literal_eval(test1)
-        file_path = os.path.join(Settings.TEST_OUT_HOME, 'results.txt')
-        with open(file_path, "a") as myfile:
-            myfile.write(str(record)+"\n")
+        file_path = os.path.join(Settings.TEST_RUN_HOME, 'results.json')
+        preserved_data = Market.get_preserved_data()
+        tempSampleStatus = None
+        recordName = record["name"]
+        originalIndex = None
+
+        if preserved_data:
+            tempSampleStatus = next((x for x in preserved_data if x["name"] == recordName), None)
+            if tempSampleStatus != None:
+                originalIndex = preserved_data.index(tempSampleStatus)
+        else:
+            preserved_data = []
+        
+        if tempSampleStatus == None:
+            tempSampleStatus = {
+                "name": recordName,
+                "core": None,
+                "angular": None,
+                "vue": None
+            }
+        
+        if record["flavor"] == "core":
+            tempSampleStatus["core"] = Market.serialize(Market.get_flavor_status(record))
+
+        if record["flavor"] == "angular":
+            tempSampleStatus["angular"] = Market.serialize(Market.get_flavor_status(record))
+
+        if record["flavor"] == "vue":
+            tempSampleStatus["vue"] = Market.serialize(Market.get_flavor_status(record))
+
+        if originalIndex == None:
+            preserved_data.append(tempSampleStatus)
+        else:
+            preserved_data[originalIndex] = tempSampleStatus
+
+        with open(file_path, "w") as jsonFile:
+            json.dump(preserved_data, jsonFile, indent=4)
+
         Log.info("++++============DATA==============++++")
         Log.info(record["name"])
         Log.info("Android Pass: " + record["android"])
         Log.info("iOS Pass: " + record["ios"])
         Log.info("++++============END==============++++")
 
+    def get_flavor_status(record):
+        tempFlavorStatus = FlavorStatus()
+        tempFlavorStatus.setAndroid(Market.convert_to_bool(record["android"]))
+        tempFlavorStatus.setIOS(Market.convert_to_bool(record["ios"]))
+        tempFlavorStatus.setSlow(Market.convert_to_bool(record["slow"]))
+
+        return tempFlavorStatus
+
+    def convert_to_bool(value):
+        return value.lower() in ("yes", "true", "t", "1")
+
     @staticmethod
     def remove_results_file():
-        file_path = os.path.join(Settings.TEST_OUT_HOME, 'results.txt')
+        file_path = os.path.join(Settings.TEST_RUN_HOME, 'results.txt')
         if os.path.exists(file_path):
             os.remove(file_path)
