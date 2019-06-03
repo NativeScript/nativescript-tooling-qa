5051b66103b8d54b4bc6105a3f439f92098f2d64
miroslavaivanova <miroslava.ivanova@progress.com>
2019-06-03 12:13:34 +0300

    create plugin tests

--- tests/cli/plugin/plugin_create_tests.py
+++ tests/cli/plugin/plugin_create_tests.py
@@ -1,59 +1,52 @@
 import os
-import unittest
 
 from core.base_test.tns_test import TnsTest
 from core.enums.os_type import OSType
-from core.enums.platform_type import Platform
 from core.settings import Settings
-from core.utils.device.adb import Adb
-from core.utils.file_utils import File, Folder
-from core.utils.npm import Npm
-from data.apps import Apps
+from core.utils.file_utils import Folder
 from products.nativescript.tns import Tns
-from products.nativescript.tns_paths import TnsPaths
 
 
-class PluginTests(TnsTest):
-    app_name = Settings.AppName.DEFAULT
-    app_path = TnsPaths.get_app_path(app_name=app_name)
-    app_temp_path = os.path.join(Settings.TEST_RUN_HOME, 'data', 'temp', 'TestApp')
+class PluginCreateTests(TnsTest):
     plugin_name = "nativescript-test-plugin"
 
     @classmethod
     def setUpClass(cls):
         TnsTest.setUpClass()
-        Tns.create(cls.app_name, app_data=Apps.MIN_JS, update=False)
-        Tns.platform_add_android(cls.app_name, framework_path=Settings.Android.FRAMEWORK_PATH)
-        if Settings.HOST_OS is OSType.OSX:
-            Tns.platform_add_ios(cls.app_name, framework_path=Settings.IOS.FRAMEWORK_PATH)
-        Folder.copy(cls.app_path, cls.app_temp_path)
 
     def setUp(self):
         TnsTest.setUp(self)
-        Folder.clean(self.app_path)
-        Folder.copy(self.app_temp_path, self.app_path)
 
     def tearDown(self):
         TnsTest.tearDown(self)
+        Folder.clean(self.plugin_name)
+        Folder.clean("plugin-folder")
 
     @classmethod
     def tearDownClass(cls):
         TnsTest.tearDownClass()
-        Folder.clean(cls.app_temp_path)
+        Folder.clean(cls.plugin_name)
 
-    def test_100_plugin_add_after_platform_add_android(self):
-        Tns.pl
-        result = Tns.plugin_add(plugin_name='tns-plugin', path=self.app_name)
-        assert 'Successfully installed plugin tns-plugin' in result.output
-        assert File.exists(os.path.join(TnsPaths.get_app_node_modules_path(self.app_name), 'tns-plugin', 'index.js'))
-        assert File.exists(os.path.join(TnsPaths.get_app_node_modules_path(self.app_name), 'tns-plugin',
-                                        'package.json'))
+    def test_100_plugin_create(self):
+        Tns.plugin_create(plugin_name=self.plugin_name)
 
-        output = File.read(os.path.join(self.app_path, 'package.json'))
-        assert 'org.nativescript.TestApp' in output
-        assert 'dependencies' in output
-        assert 'tns-plugin' in output
+    def test_200_plugin_create_with_path(self):
+        Tns.plugin_create(plugin_name=self.plugin_name, path="plugin-folder", verify=False)
 
+    def test_201_plugin_create_custom_template(self):
+        result = Tns.exec_command(command="plugin create --includeTypeScriptDemo=y nativescript-test-plugin "
+                                 "--template https://github.com/NativeScript/nativescript-plugin-seed/tarball/master")
+        assert "Make sure your custom template is compatible with the Plugin Seed" in result.output
 
+    def test_202_plugin_create_custom_user_and_plugin_name(self):
+        Tns.exec_command(command="plugin create --includeTypeScriptDemo=y nativescript-test-plugin "
+                                 "--username gitUser --pluginName customName")
+        # TODO: Assert username and pluginName are replaced in generated files.
 
-
+    def test_300_build_demo(self):
+        # TODO: Run npm scripts from plugin seed (build plugin, link plugin and then build the app).
+        Tns.plugin_create(self.plugin_name)
+        demo_path = os.path.join(self.plugin_name, 'demo')
+        Tns.build_android(demo_path, bundle=True)
+        if Settings.HOST_OS is OSType.OSX:
+            Tns.build_ios(demo_path, bundle=True)
