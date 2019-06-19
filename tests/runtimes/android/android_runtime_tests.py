8331c4b140fdfff0bbd18534b7170fbf8ab2b090
vhristov5555 <vasil.hristov@progress.com>
2019-06-19 11:28:02 +0300

    Add test for removing warnings from builds.

--- tests/runtimes/android/android_runtime_tests.py
+++ tests/runtimes/android/android_runtime_tests.py
@@ -77,6 +77,22 @@ class AndroidRuntimeTests(TnsTest):
         # make sure we called custom activity declared in manifest
         assert assert_result, "Expected output not found! Logs: " + Adb.get_logcat(device_id=self.emulator.id)
 
+    def test_250_check_plugin_dependencies_are_found(self):
+        """
+         Test that all plugin's dependencies are found
+         https://github.com/NativeScript/android-runtime/issues/1379
+        """
+        Tns.plugin_add(plugin_name="nativescript-image", path=os.path.join(TEST_RUN_HOME, APP_NAME))
+        log = Tns.build_android(os.path.join(TEST_RUN_HOME, APP_NAME))
+
+        strings = ['WARNING: Skipping interface',
+                   'as it cannot be resolved']
+
+        test_result = Wait.until(lambda: all(string not in log.output for string in strings),
+                             timeout=10, period=5)
+
+        assert test_result, 'It seems that not all plugin\'s dependencies are found!'
+
     def test_300_verbose_log_android(self):
         File.copy(os.path.join(TEST_RUN_HOME, 'assets', 'runtime', 'android', 'files', 'verbose_log', 'app.js'),
                   os.path.join(TEST_RUN_HOME, APP_NAME, 'app', 'app.js'), True)
