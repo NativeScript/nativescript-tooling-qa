4d924f51130f1cf492d639c93d950f38af65f21d
Vasil Hristov <vasil.hristov@progress.com>
2019-06-25 16:43:43 +0300

    Add test for android runtime issue 1354 and 1359. Fix tests caused by bundle. (#237)

--- assets/runtime/android/files/android-runtime-1119/app.js
+++ assets/runtime/android/files/android-runtime-1119/app.js
@@ -2,7 +2,9 @@ var application = require("application");
 
 global.__onDiscardedError = function(error){
     console.log(error.message);
+    console.log("### Stack Trace Start");
     console.log(error.stackTrace);
+    console.log("### Stack Trace End");
     console.log(error.nativeException);
 }
 
---
+++ assets/runtime/android/files/android-runtime-1359/main-view-model.js
@@ -0,0 +1,25 @@
+const Observable = require("tns-core-modules/data/observable").Observable;
+
+function getMessage(counter) {
+    if (counter <= 0) {
+        return "Hoorraaay! You unlocked the NativeScript clicker achievement!";
+    } else {
+        return `${counter} taps left`;
+    }
+}
+
+function createViewModel() {
+    const viewModel = new Observable();
+    viewModel.counter = 42;
+    viewModel.message = getMessage(viewModel.counter);
+
+    viewModel.onTap = () => {
+        viewModel.counter--;
+        viewModel.set("message", getMessage(viewModel.counter));
+        throw new Error("test!");
+    };
+
+    return viewModel;
+}
+
+exports.createViewModel = createViewModel;
