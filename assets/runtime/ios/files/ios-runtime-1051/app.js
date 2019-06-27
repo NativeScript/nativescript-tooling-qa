d01d1d8608de70d74babe6c63ef6a7cc5f25c8f7
Natalia-Hristova <natalia.hristova@telerik.com>
2019-06-27 18:42:25 +0300

    fix: update files in ios test

--- assets/runtime/ios/files/ios-runtime-1051/app.js
+++ assets/runtime/ios/files/ios-runtime-1051/app.js
@@ -1,4 +1,10 @@
-var application = require("application");
+/*
+In NativeScript, the app.js file is the entry point to your application.
+You can use this file to perform app-level initialization, but the primary
+purpose of the file is to pass control to the app’s first module.
+*/
+
+const application = require("tns-core-modules/application");
 
 global.__onDiscardedError = function(error){
     console.log(error.message);
@@ -6,4 +12,9 @@ global.__onDiscardedError = function(error){
     console.log(error.nativeException);
 }
 
-application.start({ moduleName: "main-page" });
+application.run({ moduleName: "app-root" });
+
+/*
+Do not place any code after the application has been started as it will not
+be executed on iOS.
+*/
