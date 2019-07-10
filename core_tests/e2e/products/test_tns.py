6847862d95ad4818460c8d0e492d835c409495c8
dtopuzov <dtopuzov@gmail.com>
2019-07-10 01:48:28 -0700

    fix: lint

--- core_tests/e2e/products/test_tns.py
+++ core_tests/e2e/products/test_tns.py
@@ -1,17 +1,26 @@
 import unittest
 
+from selenium.webdriver.common.by import By
+
 from core.base_test.tns_run_test import TnsRunTest
 from core.enums.os_type import OSType
 from core.enums.platform_type import Platform
 from core.settings import Settings
+from core.utils.appium.appium_driver import AppiumDriver
+from data.changes import Changes
 from data.sync.hello_world_js import sync_hello_world_js
 from data.templates import Template
+from products.nativescript.run_type import RunType
 from products.nativescript.tns import Tns
+from products.nativescript.tns_logs import TnsLogs
+from products.nativescript.tns_paths import TnsPaths
 
 APP_NAME = Settings.AppName.DEFAULT
+BUNDLE_ID = TnsPaths.get_bundle_id(APP_NAME)
 
 
 class TnsSmokeTests(TnsRunTest):
+    appium = None
 
     @classmethod
     def setUpClass(cls):
@@ -33,7 +42,24 @@ class TnsSmokeTests(TnsRunTest):
 
     def test_001_run_android_js(self):
         sync_hello_world_js(app_name=APP_NAME, platform=Platform.ANDROID, device=self.emu, instrumented=False)
+        self.__test_appium(platform=Platform.ANDROID, device=self.emu)
 
     @unittest.skipIf(Settings.HOST_OS is not OSType.OSX, 'iOS tests can be executed only on macOS.')
     def test_002_run_ios_js(self):
         sync_hello_world_js(app_name=APP_NAME, platform=Platform.IOS, device=self.sim, instrumented=False)
+        self.__test_appium(platform=Platform.IOS, device=self.sim)
+
+    def __test_appium(self, platform, device):
+        result = Tns.run(app_name=APP_NAME, platform=platform, emulator=True, wait=False)
+        strings = TnsLogs.run_messages(app_name=APP_NAME, platform=platform, run_type=RunType.UNKNOWN)
+        TnsLogs.wait_for_log(log_file=result.log_file, string_list=strings, timeout=240)
+        device.wait_for_text(text=Changes.JSHelloWord.JS.old_text)
+        device.wait_for_text(text=Changes.JSHelloWord.XML.old_text)
+
+        self.appium = AppiumDriver(platform=platform, device=device, bundle_id=BUNDLE_ID)
+        if platform == Platform.ANDROID:
+            self.appium.driver.find_element(By.XPATH, "//*[@text='TAP']").click()
+            assert self.appium.driver.find_element(By.XPATH, "//*[@text='41 taps left']").is_displayed()
+        if platform == Platform.IOS:
+            self.appium.driver.find_element(By.ID, 'TAP').click()
+            assert self.appium.driver.find_element(By.ID, '41 taps left').is_displayed()
