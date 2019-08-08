598414d177c4f3c39d50923d75231e050c19f981
vhristov5555 <vasil.hristov@progress.com>
2019-08-08 15:24:32 +0300

    Fix preview tests.

--- core_tests/unit/product/test_preview_helpers.py
+++ core_tests/unit/product/test_preview_helpers.py
@@ -1,16 +1,26 @@
 import os
 import unittest
 
-from core.utils.file_utils import File
+from core.settings.Settings import TEST_RUN_HOME, AppName
+from core.utils.file_utils import Folder, File
+
+from data.templates import Template
 from products.nativescript.preview_helpers import Preview
+from products.nativescript.tns import Tns
 
 
 class PreviewHelperTests(unittest.TestCase):
     current_folder = os.path.dirname(os.path.realpath(__file__))
+    log = ""
+
+    @classmethod
+    def setUpClass(cls):
+        Folder.clean(os.path.join(TEST_RUN_HOME, AppName.DEFAULT))
+        Tns.create(app_name=AppName.DEFAULT, template=Template.HELLO_WORLD_JS.local_package, update=True)
+        cls.log = File.read(Tns.preview(AppName.DEFAULT).log_file)
 
     def test_01_constants(self):
-        text = File.read(path=os.path.join(self.current_folder, 'preview.log'))
-        url = Preview.get_url(output=text)
+        url = Preview.get_url(output=self.log)
         assert 'nsplay://boot?instanceId=' in url
 
 
