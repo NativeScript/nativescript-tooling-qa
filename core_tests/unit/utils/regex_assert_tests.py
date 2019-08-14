62c4beddf2f8c94ec95b2208e3f8c71b156b408a
vhristov5555 <vasil.hristov@progress.com>
2019-08-14 15:54:35 +0300

    Add more cases.

--- core_tests/unit/utils/regex_assert_tests.py
+++ core_tests/unit/utils/regex_assert_tests.py
@@ -9,10 +9,21 @@ class RegexAssertTests(unittest.TestCase):
         expected_number = "33"
         actual_data = "test " + expected_number + " test"
         expected_data = r"test \d+ test"
+        case_actual_data = "TEST " + expected_number + " TEST"
+        multi_line_actual_data = """
+        test
+        test
+        test 33 test
+        test
+        test
+        """
         RegexAssert.regex_assert(actual_data, expected_data)
         RegexAssert.regex_assert(actual_data, actual_data)
         RegexAssert.regex_assert(actual_data, expected_number)
         RegexAssert.regex_assert(actual_data, r"\d+")
+        RegexAssert.regex_assert(case_actual_data, actual_data)
+        RegexAssert.regex_assert(multi_line_actual_data, r"\d+")
+        RegexAssert.regex_assert(multi_line_actual_data, actual_data)
 
     def test_assert_with_regex_fails_without_error_message(self):
         expected_number = "dd"
