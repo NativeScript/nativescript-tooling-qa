08a8c14ae5d45855e19edb5f4eb9950a123d5e67
endarova <maria.endarova@telerik.com>
2019-09-13 16:29:29 +0300

    fix lint

--- core/utils/chrome/chrome_dev_tools.py
+++ core/utils/chrome/chrome_dev_tools.py
@@ -1,7 +1,5 @@
 from time import sleep
 
-import pyautogui
-pyautogui.FAILSAFE = False
 from aenum import IntEnum
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
@@ -12,6 +10,9 @@ from core.enums.platform_type import Platform
 from core.log.log import Log
 from core.settings import Settings
 from core.utils.wait import Wait
+
+import pyautogui
+pyautogui.FAILSAFE = False
 
 
 class ChromeDevToolsTabs(IntEnum):
