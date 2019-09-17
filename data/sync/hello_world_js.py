609bc4569c089dc8a7811be27a35d943be47628a
Miroslava Ivanova <miroslava.ivanova@progress.com>
2019-09-17 16:41:11 +0300

    increase timeout

--- data/sync/hello_world_js.py
+++ data/sync/hello_world_js.py
@@ -147,7 +147,7 @@ def preview_hello_world_js_ts(app_name, platform, device, bundle=True, hmr=True,
 
     # Verify app looks properly
     device.wait_for_text(text=Changes.JSHelloWord.JS.old_text, timeout=60, retry_delay=5)
-    device.wait_for_text(text=Changes.JSHelloWord.XML.old_text, timeout=30)
+    device.wait_for_text(text=Changes.JSHelloWord.XML.old_text, timeout=60)
     device.wait_for_main_color(color=Colors.WHITE)
     initial_state = os.path.join(Settings.TEST_OUT_IMAGES, device.name, 'initial_state.png')
     device.get_screen(path=initial_state)
