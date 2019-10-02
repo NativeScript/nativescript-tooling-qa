2e739adbea04371c62459206d9aa47e8a3f0a55f
endarova <maria.endarova@telerik.com>
2019-10-02 16:39:01 +0300

    update run messages

--- tests/cli/run/tests/run_tests.py
+++ tests/cli/run/tests/run_tests.py
@@ -306,10 +306,8 @@ class TnsRunJSTests(TnsRunTest):
 
         # Delete file
         File.delete(renamed_file)
-        strings = ["Module build failed", "Error: ENOENT: no such file or directory",
-                   'Successfully synced application']
+        strings = ["Module build failed", "Error: ENOENT: no such file or directory"]
         TnsLogs.wait_for_log(log_file=result.log_file, string_list=strings)
-        self.emu.wait_for_text(text='Exception')
 
         File.replace(app_js_file, "require('./test_2.js');", ' ')
         strings = TnsLogs.run_messages(app_name=self.app_name, platform=Platform.ANDROID,
@@ -332,7 +330,6 @@ class TnsRunJSTests(TnsRunTest):
         Folder.clean(folder_name)
         strings = ["Module build failed", "Error: ENOENT: no such file or directory"]
         TnsLogs.wait_for_log(log_file=result.log_file, string_list=strings)
-        self.emu.wait_for_text(text='Exception')
 
     @unittest.skipIf(Settings.HOST_OS != OSType.OSX, 'iOS tests can be executed only on macOS.')
     def test_115_tns_run_ios_add_remove_files_and_folders(self):
@@ -364,8 +361,7 @@ class TnsRunJSTests(TnsRunTest):
 
         # Delete file
         File.delete(renamed_file)
-        strings = ["Module build failed", "Error: ENOENT: no such file or directory",
-                   "NativeScript debugger detached"]
+        strings = ["Module build failed", "Error: ENOENT: no such file or directory"]
         TnsLogs.wait_for_log(log_file=result.log_file, string_list=strings)
 
         File.replace(app_js_file, "require('./test_2.js');", ' ')
