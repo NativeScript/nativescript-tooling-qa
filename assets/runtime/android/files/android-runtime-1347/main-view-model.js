c7977ac967671f2b07df641564ac2aee0f920f7f
vhristov5555 <vasil.hristov@progress.com>
2019-10-04 12:15:09 +0300

    Fix service tests for api29.

--- assets/runtime/android/files/android-runtime-1347/main-view-model.js
+++ assets/runtime/android/files/android-runtime-1347/main-view-model.js
@@ -19,9 +19,19 @@ function createViewModel() {
     viewModel.onTap = () => {
         viewModel.counter--;
         viewModel.set("message", getMessage(viewModel.counter));
-                if (platformModule.device.sdkVersion >= "26") {
-            let intent = new android.content.Intent(app.android.context, com.nativescript.location.BackgroundService.class);
-            app.android.context.startForegroundService(intent);
+            if (platformModule.device.sdkVersion >= "26") {
+
+                var notificationManager = app.android.context.getSystemService(android.content.Context.NOTIFICATION_SERVICE);
+
+                var notificationChannel = new android.app.NotificationChannel(global.channelId, global.channelId, android.app.NotificationManager.IMPORTANCE_DEFAULT);
+
+                notificationChannel.setDescription(global.channelId);
+                notificationChannel.setSound(null, null);
+
+                notificationManager.createNotificationChannel(notificationChannel);
+
+                let intent = new android.content.Intent(app.android.context, com.nativescript.location.BackgroundService.class);
+                app.android.context.startForegroundService(intent);
         } else {
             let intent = new android.content.Intent(app.android.context, com.nativescript.location.BackgroundService.class);
             app.android.context.startService(intent);
