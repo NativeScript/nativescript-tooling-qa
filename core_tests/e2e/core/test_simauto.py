819851dc9becc0dcb728f927978007a740ff2e22
Dimitar Topuzov <dtopuzov@gmail.com>
2019-10-10 23:04:50 +0300

    feat: make simauto compatible with iOS13 (#396)
    * feat: make simauto compatible with iOS13
    
    - Make `Simauto` compatible with iOS 13.
    - Add test for it
    
    * chore: fix lint error

---
+++ core_tests/e2e/core/test_simauto.py
@@ -0,0 +1,23 @@
+from core.base_test.tns_run_ios_test import TnsRunIOSTest
+
+
+class SimAutoTests(TnsRunIOSTest):
+    apk_path = None
+
+    @classmethod
+    def setUpClass(cls):
+        TnsRunIOSTest.setUpClass()
+
+    def setUp(self):
+        TnsRunIOSTest.setUp(self)
+
+    def tearDown(self):
+        TnsRunIOSTest.tearDown(self)
+
+    @classmethod
+    def tearDownClass(cls):
+        TnsRunIOSTest.tearDownClass()
+
+    def test_01_sim_auto_smoke_test(self):
+        self.sim.wait_for_text(text="Calendar")
+        self.sim.wait_for_text(text="Photos")
