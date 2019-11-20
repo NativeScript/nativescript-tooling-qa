e8626e71dc4c61f4e6d2ddf86d415970ce7b4ccb
Miroslava Ivanova <miroslava.ivanova@progress.com>
2019-11-20 11:21:33 +0200

    perf tests

--- products/nativescript/perf_helpers.py
+++ products/nativescript/perf_helpers.py
@@ -6,11 +6,13 @@ from core.utils.file_utils import Folder, File
 from core.utils.gradle import Gradle
 from core.utils.json_utils import JsonUtils
 from core.utils.npm import Npm
+from core.utils.perf_utils import PerfUtils
 from core.utils.xcode import Xcode
 from data.changes import Sync
 from products.nativescript.tns import Tns
 
 RETRY_COUNT = 3
+TOLERANCE = 0.20
 APP_NAME = Settings.AppName.DEFAULT
 EXPECTED_RESULTS = JsonUtils.read(os.path.join(Settings.TEST_RUN_HOME, 'tests', 'perf', 'data.json'))
 
@@ -79,3 +81,48 @@ class Helpers(object):
     def get_expected_result(template, platform, entry):
         platform = str(platform)
         return EXPECTED_RESULTS[template][platform][entry]
+
+    @staticmethod
+    def prepare_data(template, template_package, change_set):
+        android_result_file = Helpers.get_result_file_name(template, Platform.ANDROID)
+        ios_result_file = Helpers.get_result_file_name(template, Platform.IOS)
+        Helpers.prepare_and_build(template=template_package, platform=Platform.ANDROID,
+                                  change_set=change_set, result_file=android_result_file)
+        Helpers.prepare_and_build(template=template_package, platform=Platform.IOS,
+                                  change_set=change_set, result_file=ios_result_file)
+
+    @staticmethod
+    def prepare_android_initial(template):
+        actual = Helpers.get_actual_result(template, Platform.ANDROID, 'prepare_initial')
+        expected = Helpers.get_expected_result(template, Platform.ANDROID, 'prepare_initial')
+        assert PerfUtils.is_value_in_range(actual, expected, TOLERANCE), 'Initial android prepare time is not OK.'
+
+    @staticmethod
+    def prepare_ios_initial(template):
+        actual = Helpers.get_actual_result(template, Platform.IOS, 'prepare_initial')
+        expected = Helpers.get_expected_result(template, Platform.IOS, 'prepare_initial')
+        assert PerfUtils.is_value_in_range(actual, expected, TOLERANCE), 'Initial ios prepare time is not OK.'
+
+    @staticmethod
+    def build_android_initial(template):
+        actual = Helpers.get_actual_result(template, Platform.ANDROID, 'build_initial')
+        expected = Helpers.get_expected_result(template, Platform.ANDROID, 'build_initial')
+        assert PerfUtils.is_value_in_range(actual, expected, TOLERANCE), 'Initial android build time is not OK.'
+
+    @staticmethod
+    def build_ios_initial(template):
+        actual = Helpers.get_actual_result(template, Platform.IOS, 'build_initial')
+        expected = Helpers.get_expected_result(template, Platform.IOS, 'build_initial')
+        assert PerfUtils.is_value_in_range(actual, expected, TOLERANCE), 'Initial ios build time is not OK.'
+
+    @staticmethod
+    def build_android_incremental(template):
+        actual = Helpers.get_actual_result(template, Platform.ANDROID, 'build_incremental')
+        expected = Helpers.get_expected_result(template, Platform.ANDROID, 'build_incremental')
+        assert PerfUtils.is_value_in_range(actual, expected, TOLERANCE), 'Incremental android build time is not OK.'
+
+    @staticmethod
+    def build_ios_incremental(template):
+        actual = Helpers.get_actual_result(template, Platform.IOS, 'build_incremental')
+        expected = Helpers.get_expected_result(template, Platform.IOS, 'build_incremental')
+        assert PerfUtils.is_value_in_range(actual, expected, TOLERANCE), 'Incremental ios build time is not OK.'
--- tests/perf/tooling/build_perf_master_detail_ng.py
+++ tests/perf/tooling/build_perf_master_detail_ng.py
@@ -7,14 +7,10 @@ from parameterized import parameterized
 
 from core.base_test.tns_test import TnsTest
 from core.enums.os_type import OSType
-from core.enums.platform_type import Platform
 from core.settings import Settings
-from core.utils.perf_utils import PerfUtils
 from data.changes import Changes
 from data.templates import Template
 from products.nativescript.perf_helpers import Helpers
-
-TOLERANCE = 0.20
 
 
 # noinspection PyMethodMayBeStatic,PyUnusedLocal
@@ -36,48 +32,31 @@ class PrepareAndBuildPerfTests(TnsTest):
 
     @parameterized.expand(TEST_DATA)
     def test_001_prepare_data(self, template, template_package, change_set):
-        android_result_file = Helpers.get_result_file_name(template, Platform.ANDROID)
-        ios_result_file = Helpers.get_result_file_name(template, Platform.IOS)
-        Helpers.prepare_and_build(template=template_package, platform=Platform.ANDROID,
-                                  change_set=change_set, result_file=android_result_file)
-        Helpers.prepare_and_build(template=template_package, platform=Platform.IOS,
-                                  change_set=change_set, result_file=ios_result_file)
+        Helpers.prepare_data(template, template_package, change_set)
 
     @parameterized.expand(TEST_DATA)
     def test_200_prepare_android_initial(self, template, template_package, change_set):
-        actual = Helpers.get_actual_result(template, Platform.ANDROID, 'prepare_initial')
-        expected = Helpers.get_expected_result(template, Platform.ANDROID, 'prepare_initial')
-        assert PerfUtils.is_value_in_range(actual, expected, TOLERANCE), 'Initial android prepare time is not OK.'
+        Helpers.prepare_android_initial(template)
 
     @parameterized.expand(TEST_DATA)
     @unittest.skipIf(Settings.HOST_OS != OSType.OSX, 'iOS tests can be executed only on macOS.')
     def test_201_prepare_ios_initial(self, template, template_package, change_set):
-        actual = Helpers.get_actual_result(template, Platform.IOS, 'prepare_initial')
-        expected = Helpers.get_expected_result(template, Platform.IOS, 'prepare_initial')
-        assert PerfUtils.is_value_in_range(actual, expected, TOLERANCE), 'Initial ios prepare time is not OK.'
+        Helpers.prepare_ios_initial(template)
 
     @parameterized.expand(TEST_DATA)
     def test_300_build_android_initial(self, template, template_package, change_set):
-        actual = Helpers.get_actual_result(template, Platform.ANDROID, 'build_initial')
-        expected = Helpers.get_expected_result(template, Platform.ANDROID, 'build_initial')
-        assert PerfUtils.is_value_in_range(actual, expected, TOLERANCE), 'Initial android build time is not OK.'
+        Helpers.build_android_initial(template)
 
     @parameterized.expand(TEST_DATA)
     @unittest.skipIf(Settings.HOST_OS != OSType.OSX, 'iOS tests can be executed only on macOS.')
     def test_301_build_ios_initial(self, template, template_package, change_set):
-        actual = Helpers.get_actual_result(template, Platform.IOS, 'build_initial')
-        expected = Helpers.get_expected_result(template, Platform.IOS, 'build_initial')
-        assert PerfUtils.is_value_in_range(actual, expected, TOLERANCE), 'Initial ios build time is not OK.'
+        Helpers.build_ios_initial(template)
 
     @parameterized.expand(TEST_DATA)
     def test_310_build_android_incremental(self, template, template_package, change_set):
-        actual = Helpers.get_actual_result(template, Platform.ANDROID, 'build_incremental')
-        expected = Helpers.get_expected_result(template, Platform.ANDROID, 'build_incremental')
-        assert PerfUtils.is_value_in_range(actual, expected, TOLERANCE), 'Incremental android build time is not OK.'
+        Helpers.build_android_incremental(template)
 
     @parameterized.expand(TEST_DATA)
     @unittest.skipIf(Settings.HOST_OS != OSType.OSX, 'iOS tests can be executed only on macOS.')
     def test_311_build_ios_incremental(self, template, template_package, change_set):
-        actual = Helpers.get_actual_result(template, Platform.IOS, 'build_incremental')
-        expected = Helpers.get_expected_result(template, Platform.IOS, 'build_incremental')
-        assert PerfUtils.is_value_in_range(actual, expected, TOLERANCE), 'Incremental ios build time is not OK.'
+        Helpers.build_ios_incremental(template)
